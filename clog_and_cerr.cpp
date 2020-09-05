c801f9910ad898312c3e7445d9584db8af92f02e
Gaurav Tewari <gauravtewari111@gmail.com>
2020-09-05 18:41:16 +0530

    basics added

---
+++ clog_and_cerr.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    clog << "An error occured";
+
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cerr << "An error occured";
+    return 0;
+}
+
---
+++ floating_number.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double x = 1.4444,y = 1.5555 ,z= 1.8888;
+    cout<<floor(x)<<"\n";//1
+    cout<<floor(y)<<"\n";
+    cout<<floor(z)<<"\n";
+    cout<<ceil(x)<<"\n";//2
+    cout<<ceil(y)<<"\n";//2
+    cout<<ceil(z)<<"\n";//2
+    cout<<trunc(x)<<"\n";//1
+    cout<<trunc(y)<<"\n";
+    cout<<trunc(z)<<"\n";
+    cout<<round(x)<<"\n";//this will round of numbers
+    cout<<round(y)<<"\n";
+    cout<<round(z)<<"\n";
+    
+    
+
+
+
+
+    
+    
+    return 0;
+}
